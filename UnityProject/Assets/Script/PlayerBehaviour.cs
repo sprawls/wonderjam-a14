0d00498b112db55dc14739cf4bf5f5b97df57fca
Frédéric Bolduc <bolducfrederic@gmail.com>
2014-10-11 01:55:57 -0400

    DJ dance

--- UnityProject/Assets/Script/PlayerBehaviour.cs
+++ UnityProject/Assets/Script/PlayerBehaviour.cs
@@ -12,23 +12,24 @@ public class PlayerBehaviour : MonoBehaviour, IBeatReceiver {
 	// Use this for initialization
 	void Start () {
 		sRenderer = GetComponent<SpriteRenderer>();
+		GameManager.Instance.requestBeat (this);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// Refresh frame
+		sRenderer.sprite = frames[currentFrame];
 	}
 	
 	public void OnBeat(BeatEnum p1, BeatEnum p2, bool turnP1) {
 		if (i < framesUntilAnim)
 			i++;
 
-		// Switch sprite frame
+		// Switch sprite frame index
 		else { 
 			i = 0;
 			currentFrame++;
 			if (currentFrame == frames.Count) currentFrame = 0;
-			sRenderer.sprite = frames[currentFrame];
 			Debug.Log ("Current frame : " + currentFrame);
 		}
 	}
