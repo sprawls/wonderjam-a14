9f3990c0aca8f6fdec8a64e9dc423d36fb83bb40
Martin Lavoie <broemartino@gmail.com>
2014-10-11 01:25:34 -0400

    Moving tile

--- UnityProject/Assets/Script/BeatManager.cs
+++ UnityProject/Assets/Script/BeatManager.cs
@@ -110,7 +110,9 @@ public class BeatManager {
             {
                 p1Turn = true;
             }
-            
+
+
+           
         }
         if (p1Turn)
         {
@@ -120,7 +122,7 @@ public class BeatManager {
         {
             IbeatReceiverRef.OnBeat(p2Input, p1Input, p1Turn);
         }
-        Debug.Log("tour de p1:"+ p1Turn+" | "+ beatCpt);
+        //Debug.Log("tour de p1:"+ p1Turn+" | "+ beatCpt);
         p1Input = BeatEnum.Missed;
         p2Input = BeatEnum.Missed;
     }
---
+++ UnityProject/Assets/Script/PlancherCreator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlancherCreator : MonoBehaviour {
+
+    public GameObject black;
+    public GameObject white;
+
+	// Use this for initialization
+	void Start () {
+        float x = 0;
+        float y = 0;
+        float z = 0;
+
+        GameManager gm = FindObjectOfType<GameManager>();
+
+        for(int i = 0; i < 40; i++)
+        {
+            GameObject go = Instantiate(i % 2 ==0 ? black : white) as GameObject;
+            TileAnimation ta = go.GetComponent<TileAnimation>();
+            gm.requestBeat(ta);
+            
+            go.transform.position = new Vector3(x, y, z);
+            go.transform.parent = transform;
+            x += 10;
+            if(x > 40)
+            {
+                x = 0;
+                z += 10;
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
