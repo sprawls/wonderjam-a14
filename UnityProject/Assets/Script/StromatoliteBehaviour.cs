833bee14578669b51f6532135777b12f9e9274fc
Alex <alex3521@hotmail.fr>
2014-10-11 15:39:30 -0400

    added Stromatolite Beat

--- UnityProject/Assets/Script/StromatoliteBehaviour.cs
+++ UnityProject/Assets/Script/StromatoliteBehaviour.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class StromatoliteBehaviour : MonoBehaviour, IBeatReceiver {
 	private float bpm;
 	private BeatManager bm;
 	private bool createNewGem = false;
+	private bool BeatSprite = false;
 	private List<Transform> gems = new List<Transform>();
 
 	public Transform gemPrefab;
@@ -33,6 +35,12 @@ public class StromatoliteBehaviour : MonoBehaviour, IBeatReceiver {
 				Destroy(g);
 			}
 		}
+		//BEat the stromatolite sprite if needed
+		if(BeatSprite == true) {
+			StartCoroutine (BeatUpAndDown());
+			BeatSprite = false;
+		}
+
 	}
 
     public void OnQuarterBeat()
@@ -42,5 +50,33 @@ public class StromatoliteBehaviour : MonoBehaviour, IBeatReceiver {
 	public void OnBeat(BeatEnum p1, BeatEnum p2, bool turnP1) {
 		bpm = bm.interval;
 		createNewGem = true;
+		BeatSprite = true;
 	}
+	public IEnumerator BeatUpAndDown(){
+		StartCoroutine (BeatTheSprite(0.2f,1.4f));
+		yield return new WaitForSeconds(0.2f);
+		StartCoroutine (BeatTheSprite(0.2f,1.4f));
+	}
+
+	public IEnumerator BeatTheSprite(float time, float targetScale){
+		Vector3 startScale = transform.localScale;
+		float step = 0f; //raw step
+		float rate = 1f/time; //amount to add to raw step
+		float smoothStep = 0f; //current smooth step
+		float lastStep = 0f; //previous smooth step
+		while(step < 1f) { // until we're done
+			step += Time.deltaTime * rate; 
+			smoothStep = step; // finding smooth step
+			transform.localScale = new Vector3 (Mathf.Lerp(startScale.x, targetScale, (smoothStep)),
+			                                    Mathf.Lerp(startScale.y, targetScale, (smoothStep)),
+			                                    Mathf.Lerp(startScale.z, targetScale, (smoothStep))); //lerp position
+			lastStep = smoothStep; //get previous last step
+			yield return null;
+		}
+		//complete rotation
+		if(step > 1.0) transform.localPosition = new Vector3 (Mathf.Lerp(startScale.x, targetScale, (1)),
+		                                                      Mathf.Lerp(startScale.y, targetScale, (1)),
+		                                                      Mathf.Lerp(startScale.z, targetScale, (1))); 
+	}
+
 }
