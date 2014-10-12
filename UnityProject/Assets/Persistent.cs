e81b5f68328e3285d01953486a363b221e4ce0f9
Alex <aDesjardinsAlex@gmail.com>
2014-10-12 11:42:06 -0400

    shitton of moe

--- UnityProject/Assets/Persistent.cs
+++ UnityProject/Assets/Persistent.cs
@@ -7,6 +7,15 @@ public class Persistent : MonoBehaviour {
     public int songBPM;
     public float songMulti;
 
+    public int OptZombiesCount=50;
+    public int OptSpeedTurn=16;
+    public int OptBaseScore=500;
+    public int OptZombieCtrl=2;
+
+    public bool OptFeverMode=false;
+    public bool OptChaosMode=false;
+    public bool OptTacticMode=false;
+
 	static bool instanceIsLoaded = false;
 
 	void OnLevelWasLoaded(int level){
@@ -19,7 +28,7 @@ public class Persistent : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		if(instanceIsLoaded == false) {
-			DontDestroyOnLoad(this);
+			DontDestroyOnLoad(this); 
 			instanceIsLoaded = true;
 			//Cap framerate
 			Application.targetFrameRate = 60;
--- UnityProject/Assets/Script/MenuManager.cs
+++ UnityProject/Assets/Script/MenuManager.cs
@@ -16,6 +16,8 @@ public class MenuManager : MonoBehaviour {
     private int SongIndice = 0;
     private float BPMModifier = 1;
 
+    private bool AddSongMenu = false;
+
     private float Swidth;
     private float Sheight;
 
@@ -38,93 +40,128 @@ public class MenuManager : MonoBehaviour {
 		Swidth=Screen.width;
 		Sheight = Screen.height;
 		GUI.skin = skinMenu;
-
+        float curWidth;
+        float curHeight;
 		// Background
 		GUI.DrawTexture (new Rect (0, 0, Swidth, Sheight), bgMenu);
 
-		// Choix du beat
-		float curWidth = 75;
-		float curHeight = 20;
-		GUI.Label (new Rect ((Swidth - curWidth) / 2.0f, (Sheight - curHeight) / 2.0f - 25, curWidth, curHeight), "Choix du beat", skinMenu.GetStyle ("BPM label"));
-
-		// Authors
-		curWidth = 900;
-		curHeight = 20;
-		GUI.Label (new Rect ((Swidth - curWidth) / 2.0f, (Sheight - curHeight) / 2.0f - 250, curWidth, curHeight), "Alex Arsenault-Desjardins            Frédéric Bolduc            Martin Lavoie            Alexis Lessard", skinMenu.GetStyle ("Auteurs"));
-
-		// Song label
-        curWidth=150;
-        curHeight=20;
-		GUIStyle songLabelStyle = skinMenu.GetStyle ("Song label");
-		songLabelStyle.normal.textColor = textColor;
-		//Debug.Log (skinMenu.GetStyle ("Song label").normal.textColor);
-		GUI.Label(new Rect((Swidth - curWidth) / 2.0f, (Sheight - curHeight) / 2.0f, curWidth, curHeight), SongNames[SongIndice], songLabelStyle);
-		
-		// Previous song
-        curWidth = 48;
-        curHeight = 48;
-        if (SongIndice > 0 && GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) - 425, (Sheight - curHeight) / 2.0f, curWidth, curHeight), "", skinMenu.GetStyle("Previous song")))
+       /* if (AddSongMenu)
         {
-            ChangeSong(-1);
+            // ajouter button
+            curWidth = 160;
+            curHeight = 60;
+            if (GUI.Button(new Rect(((Swidth - curWidth) / 2.0f), ((Sheight - curHeight) / 2.0f) + 100, curWidth, curHeight), "Ajouter", skinMenu.GetStyle("Start button")))
+            {
+
+            }
+
+            // back button
+            curWidth = 160;
+            curHeight = 60;
+            if (GUI.Button(new Rect(((Swidth - curWidth) / 2.0f), ((Sheight - curHeight) / 2.0f) + 240, curWidth, curHeight), "Retour", skinMenu.GetStyle("Start button")))
+            {
+                AddSongMenu = false;
+            }
         }
+        else
+        {*/
+            // Choix du beat
+            curWidth = 75;
+            curHeight = 20;
+            GUI.Label(new Rect((Swidth - curWidth) / 2.0f, (Sheight - curHeight) / 2.0f - 25, curWidth, curHeight), "Choix du beat", skinMenu.GetStyle("BPM label"));
 
-		// Next song
-        curWidth = 48;
-        curHeight = 48;
-		if (SongIndice+1 < PathSongs.Count && GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) + 425, (Sheight - curHeight) / 2.0f, curWidth, curHeight), "", skinMenu.GetStyle("Next song")))
-		{
-            ChangeSong(1);
-        }
+            // Authors
+            curWidth = 900;
+            curHeight = 20;
+            GUI.Label(new Rect((Swidth - curWidth) / 2.0f, (Sheight - curHeight) / 2.0f - 250, curWidth, curHeight), "Alex Arsenault-Desjardins            Frédéric Bolduc            Martin Lavoie            Alexis Lessard", skinMenu.GetStyle("Auteurs"));
 
-		// BPM label
-        curWidth = 75;
-        curHeight = 20;
-		GUI.Label(new Rect(((Swidth - curWidth) / 2.0f), ((Sheight - curHeight) / 2.0f) + 50, curWidth, curHeight), Mathf.Round((BPMSongs[SongIndice] * BPMModifier)) + " BPM", skinMenu.GetStyle("BPM label"));
-		
-		// Higher BPM
-        curWidth = 24;
-        curHeight = 24;
-		if (BPMModifier<2.0f && GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) + 75, ((Sheight - curHeight) / 2.0f) + 50, curWidth, curHeight), "", skinMenu.GetStyle("Higher BPM")))
-		{
-            ChangeBPM(0.1f);
-        }
+            // Song label
+            curWidth = 150;
+            curHeight = 20;
+            GUIStyle songLabelStyle = skinMenu.GetStyle("Song label");
+            songLabelStyle.normal.textColor = textColor;
+            //Debug.Log (skinMenu.GetStyle ("Song label").normal.textColor);
+            GUI.Label(new Rect((Swidth - curWidth) / 2.0f, (Sheight - curHeight) / 2.0f, curWidth, curHeight), SongNames[SongIndice], songLabelStyle);
 
-		// Lower BPM
-        curWidth = 24;
-        curHeight = 24;
-		if (BPMModifier > 0.1f && GUI.Button (new Rect (((Swidth - curWidth) / 2.0f) - 75, ((Sheight - curHeight) / 2.0f) + 50, curWidth, curHeight), "", skinMenu.GetStyle("Lower BPM"))) {
-			ChangeBPM (-0.1f);
-		}
+            // Previous song
+            curWidth = 48;
+            curHeight = 48;
+            if (SongIndice > 0 && GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) - 425, (Sheight - curHeight) / 2.0f, curWidth, curHeight), "", skinMenu.GetStyle("Previous song")))
+            {
+                ChangeSong(-1);
+            }
 
-		// Instructions
-		curWidth = 720;
-		curHeight = 60;
-		GUI.Label(new Rect(((Swidth - curWidth) / 2.0f), ((Sheight - curHeight) / 2.0f) + 120, curWidth, curHeight), "31 octobre 2052, la race humaine a été exterminée par la menace zombie. La musique est morte, mais la légendaire bataille entre les deux dieux du rythme, DJ Guylaine Grosse-Soirée et DJ Jerry Ox, se poursuit encore après la mort. Le combat décisif se tiendra ce soir au célèbre Zombeat Stromatolite Turbo Party Club, pour la dernière chance pour le monde de retrouver un peu de couleur. Qui triomphera?", skinMenu.GetStyle("Instructions"));
-		
-		// Start button
-        curWidth = 160;
-        curHeight = 60;
-		if (GUI.Button(new Rect(((Swidth - curWidth) / 2.0f), ((Sheight - curHeight) / 2.0f) + 240, curWidth, curHeight), "go!", skinMenu.GetStyle("Start button")))
-		{
-            StartGame();
-        }
+            // Next song
+            curWidth = 48;
+            curHeight = 48;
+            if (SongIndice + 1 < PathSongs.Count && GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) + 425, (Sheight - curHeight) / 2.0f, curWidth, curHeight), "", skinMenu.GetStyle("Next song")))
+            {
+                ChangeSong(1);
+            }
 
-		// Render logo
-		logoCamera.Render ();
+            // BPM label
+            curWidth = 75;
+            curHeight = 20;
+            GUI.Label(new Rect(((Swidth - curWidth) / 2.0f), ((Sheight - curHeight) / 2.0f) + 50, curWidth, curHeight), Mathf.Round((BPMSongs[SongIndice] * BPMModifier)) + " BPM", skinMenu.GetStyle("BPM label"));
 
-		//Fever Button 
-		curWidth = 200;
-		curHeight = 60;
-		if (GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) + 200, ((Sheight - curHeight) / 2.0f) + 240, curWidth, curHeight), "FEVER MODE", skinMenu.GetStyle("Fever Button"))) 
-		{
-			feverMode = !feverMode;
-			GUIStyle myFeverStyle = skinMenu.GetStyle("Fever Button");
-			if(feverMode){
-				myFeverStyle.normal.textColor = new Color (20f/255f,0,1);
-			} else {
-				myFeverStyle.normal.textColor = new Color (111f/255f,111f/255f,111f/255f);
-			}
-		} 
+            // Higher BPM
+            curWidth = 24;
+            curHeight = 24;
+            if (BPMModifier < 2.0f && GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) + 75, ((Sheight - curHeight) / 2.0f) + 50, curWidth, curHeight), "", skinMenu.GetStyle("Higher BPM")))
+            {
+                ChangeBPM(0.1f);
+            }
+
+            // Lower BPM
+            curWidth = 24;
+            curHeight = 24;
+            if (BPMModifier > 0.1f && GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) - 75, ((Sheight - curHeight) / 2.0f) + 50, curWidth, curHeight), "", skinMenu.GetStyle("Lower BPM")))
+            {
+                ChangeBPM(-0.1f);
+            }
+
+           /* // Add Song
+            curWidth = 120;
+            curHeight = 30;
+            if (GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) + 250, ((Sheight - curHeight) / 2.0f) + 50, curWidth, curHeight), "Ajouter un Beat", skinMenu.GetStyle("BPM label")))
+            {
+                AddSongMenu = true;
+            }*/
+
+            // Instructions
+            curWidth = 720;
+            curHeight = 60;
+            GUI.Label(new Rect(((Swidth - curWidth) / 2.0f), ((Sheight - curHeight) / 2.0f) + 120, curWidth, curHeight), "31 octobre 2052, la race humaine a été exterminée par la menace zombie. La musique est morte, mais la légendaire bataille entre les deux dieux du rythme, DJ Guylaine Grosse-Soirée et DJ Jerry Ox, se poursuit encore après la mort. Le combat décisif se tiendra ce soir au célèbre Zombeat Stromatolite Turbo Party Club, pour la dernière chance pour le monde de retrouver un peu de couleur. Qui triomphera?", skinMenu.GetStyle("Instructions"));
+
+            // Start button
+            curWidth = 160;
+            curHeight = 60;
+            if (GUI.Button(new Rect(((Swidth - curWidth) / 2.0f), ((Sheight - curHeight) / 2.0f) + 240, curWidth, curHeight), "go!", skinMenu.GetStyle("Start button")))
+            {
+                StartGame();
+            }
+
+            // Render logo
+            logoCamera.Render();
+
+            //Fever Button 
+            curWidth = 200;
+            curHeight = 60;
+            if (GUI.Button(new Rect(((Swidth - curWidth) / 2.0f) + 200, ((Sheight - curHeight) / 2.0f) + 240, curWidth, curHeight), "FEVER MODE", skinMenu.GetStyle("Fever Button")))
+            {
+                feverMode = !feverMode;
+                GUIStyle myFeverStyle = skinMenu.GetStyle("Fever Button");
+                if (feverMode)
+                {
+                    myFeverStyle.normal.textColor = new Color(20f / 255f, 0, 1);
+                }
+                else
+                {
+                    myFeverStyle.normal.textColor = new Color(111f / 255f, 111f / 255f, 111f / 255f);
+                }
+            }
+
+       // }
 
 	}
 
