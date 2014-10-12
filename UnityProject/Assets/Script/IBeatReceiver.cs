ab404f0e80c9ba1cd429e804ce1ea342e66b40eb
Alex <aDesjardinsAlex@gmail.com>
2014-10-11 20:48:55 -0400

    test

--- UnityProject/Assets/Script/IBeatReceiver.cs
+++ UnityProject/Assets/Script/IBeatReceiver.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public interface IBeatReceiver {
     void OnBeat(BeatEnum mainPlayer, BeatEnum offPlayer, bool turnP1);
     void OnQuarterBeat();
+    //void LateBeat(BeatEnum mainPlayer, BeatEnum offPlayer, bool turnP1);
 }
 
 
